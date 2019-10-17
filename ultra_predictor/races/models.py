29bd201161b3a6f0e7c36630b8d58edc8a174af7
Michal Mojek <m.mojek@gmail.com>
2019-10-17 16:35:21 +0200

    Add race model

--- ultra_predictor/races/models.py
+++ ultra_predictor/races/models.py
@@ -1,10 +1,27 @@
-from sys import modules
 from django.db import models
 
 
 class RaceGroup(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
+
+class Race(models.Model):
+    race_group = models.ForeignKey(
+        RaceGroup, on_delete=models.CASCADE, related_name="races", null=True, blank=True
+    )
+    name = models.CharField(max_length=256)
+    start_date = models.DateField()
+    distance = models.DecimalField(max_digits=6, decimal_places=2)
+    elevation_gain = models.PositiveIntegerField()
+    elevation_lost = models.PositiveIntegerField()
+    itra = models.PositiveIntegerField()
+    itra_race_id = models.PositiveIntegerField()
+    food_point = models.PositiveIntegerField()
+    time_limit = models.DecimalField(max_digits=10, decimal_places=1)
 
     def __str__(self):
         return self.name
+
