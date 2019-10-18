07b4c53ce8fd791ffacf5de8d07b39b95a5b325a
Michal Mojek <m.mojek@gmail.com>
2019-10-18 13:25:14 +0200

    Add races to admin

--- ultra_predictor/races/admin.py
+++ ultra_predictor/races/admin.py
@@ -2,4 +2,26 @@ from django.contrib import admin
 from . import models
 
 
-admin.site.register(models.RaceGroup)
+class RaceInline(admin.TabularInline):
+    model = models.Race
+
+
+class RaceGroupAdmin(admin.ModelAdmin):
+    inlines = [RaceInline]
+
+
+class RaceAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "start_date",
+        "distance",
+        "elevation_gain",
+        "elevation_lost",
+        "itra",
+        "food_point",
+        "time_limit",
+    )
+
+
+admin.site.register(models.RaceGroup, RaceGroupAdmin)
+admin.site.register(models.Race, RaceAdmin)
