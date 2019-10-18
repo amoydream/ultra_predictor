2d7461e881c8fc76ea13c5e52dbe397d7efd6956
Michal Mojek <m.mojek@gmail.com>
2019-10-18 13:08:02 +0200

    Add uuid and created_at, updated_at fields, rested_db is needed

--- ultra_predictor/users/models.py
+++ ultra_predictor/users/models.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import AbstractUser
+from ultra_predictor.core.models import DefaultModel
 from django.db.models import CharField
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
 
-class User(AbstractUser):
+class User(AbstractUser, DefaultModel):
 
     # First Name and Last Name do not cover name patterns
     # around the globe.
