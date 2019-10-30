66573d1c91761a125c2c78c00e3be3d2195b4fcc
Michal Mojek <m.mojek@gmail.com>
2019-10-30 13:28:31 +0100

    Add enduhub fetcher

---
+++ ultra_predictor/races/extras/enduhub_parser.py
@@ -0,0 +1,69 @@
+from bs4 import BeautifulSoup
+
+
+class EnduhubParser:
+    def __init__(self, html, birth_year_filter):
+        self.html = html
+        self.birth_year_filter = birth_year_filter
+        self.soup = BeautifulSoup(html, "html.parser")
+        self.has_next_page = self.check_next_page()
+
+    def results(self):
+        race_results = []
+        for row in self.soup.find_all("tr", class_="Zawody"):
+            runner_name = row.find("td", class_="name").get_text().strip()
+            race_name = row.find("td", class_="event").get_text().strip()
+            distance = row.find("td", class_="distance").get_text()
+            start_date = row.find("td", class_="date").get_text()
+            birth_year = row.find("td", class_="yob").get_text()
+            time_result = row.find("td", class_="best").get_text()
+            race_type = row.find("td", class_="sport").get_text()
+
+            race_result = dict(
+                runner_name=" ".join(runner_name.split()),
+                birth_year=birth_year,
+                race_name=" ".join(race_name.split()),
+                distance=distance,
+                start_date=start_date,
+                time_result=time_result,
+                race_type=race_type,
+            )
+            print(race_result)
+            try:
+                equal_year = self.birth_year_filter == BirthYear(birth_year)
+            except ValueError:
+                continue
+            else:
+                if equal_year:
+                    race_results.append(race_result)
+        return race_results
+
+    def check_next_page(self):
+        next_li = self.soup.select(".pagination .pages li.active")[0].findNext("li")
+        return "ostatnia" not in next_li.text
+
+
+class BirthYear:
+    def __init__(self, year):
+        if not str(year).isnumeric():
+            raise ValueError("Birth year has to be a number")
+        self.year = year
+
+    @property
+    def year(self):
+        return int(self._year)
+
+    @year.setter
+    def year(self, y):
+        self._year = y
+        if len(str(y)) == 2:
+            self._year = int(y) + 1900
+
+    def __str__(self):
+        return str(self.year)
+
+    def __eq__(self, other):
+        return self.year == other
+
+    def __repr__(self):
+        return f"BirthYear({self.year})"
---
+++ ultra_predictor/races/tests/test_endu.py
@@ -0,0 +1,66 @@
+from unittest import mock
+from unittest.mock import patch
+import pytest
+from ..extras.enduhub_parser import EnduhubParser, BirthYear
+from ..extras.enduhub_fetcher import EnduhubFetcher
+
+
+def testing_initialization_of_enduhub_parser(eduhub_page_html_1):
+    birth_year_filter = 1980
+    endu_parser = EnduhubParser(eduhub_page_html_1, birth_year_filter)
+    assert endu_parser.html == eduhub_page_html_1
+    assert endu_parser.birth_year_filter == birth_year_filter
+
+
+def test_results(eduhub_page_html_1):
+    """Test results that return  filtered results year"""
+    birth_year_filter = 1987
+    endu_parser = EnduhubParser(eduhub_page_html_1, birth_year_filter)
+    assert len(endu_parser.results()) == 3
+
+
+def test_birth_year_init():
+    birth = BirthYear(1980)
+    assert str(birth) == "1980"
+    assert birth.year == 1980
+
+
+def test_birth_year_init_short():
+    birth = BirthYear(80)
+    assert str(birth) == "1980"
+    assert birth.year == 1980
+
+
+@pytest.mark.parametrize(
+    "year, year_to_birth", [(1980, 80), (1980, "80"), (1980, "1980")]
+)
+def test_birth_year_equl(year, year_to_birth):
+    assert year == BirthYear(year_to_birth)
+
+
+def test_birth_year_non_numerical():
+    with pytest.raises(ValueError):
+        BirthYear("asd")
+
+
+@patch("ultra_predictor.races.extras.enduhub_fetcher.EnduhubFetcher.get_data")
+@patch("ultra_predictor.races.extras.enduhub_parser.EnduhubParser.check_next_page")
+def test_endu_case(patch_check_next_page, patch_download_endu, eduhub_page_html_1):
+    patch_download_endu.return_value = eduhub_page_html_1
+    patch_check_next_page.return_value = False
+    page = 1
+    all_results = []
+
+    while True:
+        endu_fetcher = EnduhubFetcher("Piotr Nowak", page)
+        endu_parser = EnduhubParser(endu_fetcher.get_data(), 1987)
+        
+        all_results += endu_parser.results()
+
+        if endu_parser.has_next_page:
+            page += 1
+        else:
+            break
+    # endu_fetcher.get_data = mock.Mock()
+    # endu_fetcher.get_data.return_value = itra_html
+    assert len(all_results) == 3
