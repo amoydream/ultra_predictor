8fd07a92460553185f10baebda17a77d934b2379
Michal Mojek <m.mojek@gmail.com>
2019-11-06 14:44:04 +0100

    Add page with all race results

--- ultra_predictor/races/views.py
+++ ultra_predictor/races/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 
 from django.views.generic import ListView, DetailView
-
-
+from django.views.generic.list import MultipleObjectMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import PredictionRaceGroup, PredictionRaceResult
 
 
-class PredictionRaceGroupListView(ListView):
+class PredictionRaceGroupListView(LoginRequiredMixin, ListView):
     model = PredictionRaceGroup
     context_object_name = "prediction_race_groups_list"
     queryset = (
@@ -16,14 +16,16 @@ class PredictionRaceGroupListView(ListView):
     )
 
 
-class PredictionRaceResultGroupListView(DetailView):
+class PredictionRaceResultGroupListView(
+    LoginRequiredMixin, DetailView, MultipleObjectMixin
+):
     model = PredictionRaceGroup
     context_object_name = "prediction_group"
+    paginate_by = 50
 
     def get_context_data(self, **kwargs):
+        self.object_list = self.get_object().all_results_of_prediction_races()
         context = super().get_context_data(**kwargs)
-        context[
-            "group_race_results"
-        ] = self.get_object().all_results_of_prediction_races()
+        _, page, _, _ = self.paginate_queryset(self.object_list, self.paginate_by)
+        context["group_race_results"] = page
         return context
-
