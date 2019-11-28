eb80ba60d60506069672bb94ebf64fe87b8798f8
Michal Mojek <m.mojek@gmail.com>
2019-11-28 16:23:13 +0100

    Event Serializers

--- ultra_predictor/api_races/serializers.py
+++ ultra_predictor/api_races/serializers.py
@@ -1,8 +1,26 @@
 from rest_framework import serializers
-from ultra_predictor.races.models import Event
+from ultra_predictor.races.models import Event, PredictionRace
+
+
+class PredictionRaceSerilazer(serializers.ModelSerializer):
+    class Meta:
+        model = PredictionRace
+        fields = [
+            "name",
+            "start_date",
+            "distance",
+            "elevation_gain",
+            "elevation_lost",
+            "itra",
+            "food_point",
+            "time_limit",
+        ]
 
 
 class EventSerializer(serializers.ModelSerializer):
+    prediction_races = PredictionRaceSerilazer(many=True, read_only=True)
+
     class Meta:
         model = Event
         fields = ["name", "start_date", "end_date", "prediction_races"]
+
