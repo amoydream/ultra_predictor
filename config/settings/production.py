f7855b359533777557f9c5fc655059bbb4ae8cfe
Michal Mojek <m.mojek@gmail.com>
2019-12-13 11:34:25 +0100

    Remove aws storage

--- config/settings/production.py
+++ config/settings/production.py
@@ -106,8 +106,8 @@ class MediaRootS3Boto3Storage(S3Boto3Storage):
 
 
 # endregion
-DEFAULT_FILE_STORAGE = "config.settings.production.MediaRootS3Boto3Storage"
-MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/media/"
+# DEFAULT_FILE_STORAGE = "config.settings.production.MediaRootS3Boto3Storage"
+# MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/media/"
 
 # TEMPLATES
 # ------------------------------------------------------------------------------
